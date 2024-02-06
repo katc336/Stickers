535ee94474169ccfa18f4e3dbf0b1e99c7d3fce7
Kat Christensen <katchristensen336@gmail.com>
2024-02-06 12:04:46 -0500

    api routes for GET all classes, add a class, and add a student

--- src/server/api.js
+++ src/server/api.js
@@ -7,4 +7,47 @@ const { PrismaClient } = require("@prisma/client");
 const { recipeBookItem, recipeBook, level, userPostedRecipe } = require('./client');
 const prisma = new PrismaClient();
 
+//<-----------------GET ALL CLASSES----------------->
+apiRouter.get("/my_classes", requireUser, async (req, res, next) => {
+    try {
+        const classes = await prisma.class.findMany({
+            where: { teacherId: req.user.id }
+        })
+        res.send(classes)
+    } catch (error) {
+        next(error)
+    }
+});
+//<-----------------ADD A CLASS----------------->
+apiRouter.post("/class", requireUser, async (req, res, next) => {
+    try {
+        const { name } = req.body
+        const newClass = await prisma.class.create({
+            data: {
+                teacher: { connect: { id: req.user.id } },
+                name: name
+            }
+        })
+        res.send(newClass)
+    } catch (error) {
+        next(error)
+    }
+});
+//<-----------------ADD STUDNETS TO A CLASS----------------->
+apiRouter.post("/class/:id/add_student", requireUser, async (req, res, next) => {
+    try {
+        const { name } = req.body
+        const id = Number(req.params.id);
+        const newStudent = await prisma.student.create({
+            data: {
+                name: name,
+                class: { connect: { classId: id } }
+            }
+        })
+        res.send(newStudent)
+    } catch (error) {
+        next(error);
+    }
+});
+
 module.exports = apiRouter;
