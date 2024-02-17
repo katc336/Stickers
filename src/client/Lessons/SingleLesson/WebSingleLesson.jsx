bfb1136123bfdd75e9b7fca3bcb40d8b2d06e9d2
Kat Christensen <katchristensen336@gmail.com>
2024-02-17 00:10:05 -0500

    Fix single lesson page layout and add key property

--- src/client/Lessons/SingleLesson/WebSingleLesson.jsx
+++ src/client/Lessons/SingleLesson/WebSingleLesson.jsx
@@ -1,6 +1,7 @@
 import Alert from "@mui/material/Alert"
 import Card from "@mui/material/Card"
 import Grid from "@mui/material/Grid"
+import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
 import TextField from "@mui/material/TextField"
 import { useState } from "react"
@@ -11,7 +12,6 @@ import NavDrawer from "../../Navigation/NavDrawer"
 const WebSingleLesson = () => {
     const { id } = useParams()
     const [addLessonObjective, setAddLessonObjective] = useState(false);
-    const [clearButton, setClearButton] = useState(true);
     const [addError, setAddError] = useState(null);
     const [objectiveName, setObjectiveName] = useState("")
 
@@ -45,45 +45,48 @@ const WebSingleLesson = () => {
     return (
         <div>
             <NavDrawer />
-            <Card sx={{ ml: 20, mr: 3 }}>
+            <Card sx={{ ml: 20, mr: 3, p: 3 }}>
                 <Typography
                     variant="h3"
                     sx={{ textAlign: "center", mx: 3 }}>
-                    { data && data.lessonName}
+                    {data && data.lessonName}
                 </Typography>
                 {addError &&
                     <Alert severity="error">There was a mistake adding this objective.</Alert>}
-                {clearButton &&
-                    <button
-                        className="details-button"
-                        onClick={() => { setAddLessonObjective(true), setClearButton(false) }}>
-                        Add Lesson Objective
-                    </button>
-                }
+                <button
+                    style={{ float: "right", marginBottom: "50px",  width: "200px" }}
+                    className="details-button"
+                    onClick={() => { setAddLessonObjective(true) }}>
+                    Add Lesson Objective
+                </button>
                 {addLessonObjective &&
-                    <form onSubmit={handleAddLessonObjective}>
-                        <TextField
-                            multiline
-                            label="Lesson Objective"
-                            value={objectiveName}
-                            onChange={(event) => setObjectiveName(event.target.value)}
-                            variant="filled"
-                            sx={{ my: 1 }} />
-                        <button
-                            className="submit-button"
-                            type="submit">
-                            Add Lesson Objective
-                        </button>
-                    </form>}
+                    <div style={{ float: "right" }}>
+                        <form onSubmit={handleAddLessonObjective}>
+                            <TextField
+                                multiline
+                                label="Lesson Objective"
+                                value={objectiveName}
+                                onChange={(event) => setObjectiveName(event.target.value)}
+                                variant="filled"
+                                sx={{ m: 3, width: 900 }} />
+                            <button
+                                className="submit-button"
+                                type="submit">
+                                Add Lesson Objective
+                            </button>
+                        </form>
+                    </div>}
                 <Grid container>
                     <Grid item xs={6}>
                         <Card
                             elevation={10}
                             sx={{ m: 1, p: 1 }}>
                             {data.learningObjectives.map((objective) => (
-                                <Typography>
-                                    {objective.objectiveName}
-                                </Typography>
+                                <div key={objective.id}>
+                                    <Typography>
+                                        {objective.objectiveName}
+                                    </Typography>
+                                </div>
                             ))}
                         </Card>
                     </Grid>
@@ -91,7 +94,6 @@ const WebSingleLesson = () => {
                         <Card
                             elevation={10}
                             sx={{ m: 1, p: 1 }}>
-
                         </Card>
                     </Grid>
                 </Grid>
