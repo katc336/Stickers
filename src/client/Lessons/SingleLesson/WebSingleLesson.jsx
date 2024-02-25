17e2ffbfaad3be6a1fbaacb4dd4e51e0046a07fc
Kat Christensen <katchristensen336@gmail.com>
2024-02-25 01:40:14 -0500

    Break up components for students progress and add progress in SingleLesson

--- src/client/Lessons/SingleLesson/WebSingleLesson.jsx
+++ src/client/Lessons/SingleLesson/WebSingleLesson.jsx
@@ -1,58 +1,24 @@
-import Alert from "@mui/material/Alert"
 import Card from "@mui/material/Card"
 import Grid from "@mui/material/Grid"
-import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
-import TextField from "@mui/material/TextField"
-import { useState } from "react"
 import { useGetSingleLessonQuery, usePostNewObjectiveMutation, usePostProgressMutation, useGetAllObjectivesQuery, useDeleteObjectiveMutation } from "../../../redux/api"
 import { useParams } from "react-router-dom";
 import NavDrawer from "../../Navigation/NavDrawer"
 import ClassLessonObjective from "./components/ClassLessonObjective"
 import AddLesson from "./components/AddLesson"
+import ClassStudentsProgress from "./components/ClassStudentsProgress";
 
 const WebSingleLesson = () => {
     const { id } = useParams()
-    const [selectedStudentId, setSelectedStudentId] = useState("");
-    const [selectedObjectiveId, setSelectedObjectiveId] = useState("");
-    const [selectedCombinedObjectiveId, setSelectedCombinedObjectiveId] = useState("");
-    const [addError, setAddError] = useState(null);
-    const [progress, setProgress] = useState("");
-
     const { data, error, isLoading } = useGetSingleLessonQuery(id);
-    const { data: objData, error: objError, isLoading: objLoading } = useGetAllObjectivesQuery();
 
-    const [addProgressMutation] = usePostProgressMutation();
-    const [deleteObjective] = useDeleteObjectiveMutation();
-    if (isLoading || objLoading) {
+    if (isLoading) {
         return <div></div>
     }
-    if (error || objError) {
+    if (error) {
         console.error(error)
     }
     console.log(data)
-
-    const handleAddProgress = async (event) => {
-        try {
-            event.preventDefault();
-            const result = await addProgressMutation({
-                studentId: Number(selectedStudentId),
-                objectiveId: Number(selectedObjectiveId),
-                progressPercent: Number(progress),
-                combinedObjectiveId: Number(selectedCombinedObjectiveId)
-            });
-            if (result.data) {
-                setAddError(false);
-                setSelectedStudentId(null);
-                console.log("Success!");
-            } else {
-                setAddError(true);
-                console.log("Could not add progress");
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
     return (
         <div>
             <NavDrawer />
@@ -62,111 +28,13 @@ const WebSingleLesson = () => {
                     sx={{ textAlign: "center", mx: 3 }}>
                     {data && data.lessonName}
                 </Typography>
-                <AddLesson  id={id}/>
+                <AddLesson id={id} />
                 <Grid container>
                     <Grid item xs={6}>
                         <ClassLessonObjective id={id} />
                     </Grid>
                     <Grid item xs={6}>
-                        <Card
-                            elevation={10}
-                            sx={{ m: 1, p: 1 }}>
-                            <Typography
-                                variant="h5"
-                                sx={{ textAlign: "center", my: 1 }}>
-                                Students:
-                            </Typography>
-                            {data.class.students.map((student) => (
-                                <div key={student.id}>
-                                    <Card
-                                        elevation={10}
-                                        sx={{ p: 1, m: 1 }}>
-                                        <Stack justifyContent="space-between">
-                                            <Typography variant="h6">
-                                                {student.name}
-                                            </Typography>
-                                            {student.studentProgress.map((progress) => (
-                                                <div key={progress.id}>
-                                                    <Card
-                                                        sx={{
-                                                            p: 1,
-                                                            border: 1,
-                                                            borderColor: progress.progressPrecent < 70 ? "red" : progress.progressPrecent >= 70 && progress.progressPrecent <= 80 ? "orange" : progress.progressPrecent >= 81 && progress.progressPrecent <= 89 ? "yellow" : "green",
-                                                            backgroundColor: progress.progressPrecent < 70 ? "#FEA1A1" : progress.progressPrecent >= 70 && progress.progressPrecent <= 80 ? "#FFC97C" : progress.progressPrecent >= 81 && progress.progressPrecent <= 89 ? "#F9DE79" : "#CDE990"
-                                                        }}>
-                                                        <Stack direction="row">
-                                                            <Typography
-                                                                sx={{ mx: 1 }}>
-                                                                {progress.learningObjective.objectiveName}:
-                                                            </Typography>
-                                                            <Typography
-                                                                sx={{ mx: 1 }}>
-                                                                {progress.progressPrecent}%
-                                                            </Typography>
-                                                        </Stack>
-                                                    </Card>
-                                                </div>
-                                            ))}
-                                            <Stack direction="column">
-                                                <button
-                                                    style={{ width: 170 }}
-                                                    onClick={() => { setSelectedStudentId(student.id) }}
-                                                    className="details-button">
-                                                    Add New Progress
-                                                </button>
-                                                {selectedStudentId === student.id &&
-                                                    <Card
-                                                        sx={{ p: 1, m: 3 }}
-                                                        elevation={10}>
-                                                        <form onSubmit={handleAddProgress}>
-                                                            <Stack direction="column">
-                                                                <Typography variant="h5" sx={{ my: 1 }}>
-                                                                    Select Learning Objective:
-                                                                </Typography>
-                                                                {data.learningObjectives.map((objective) => (
-                                                                    <div key={objective.id}>
-                                                                        <Stack direction="row">
-                                                                            <input
-                                                                                type="checkbox"
-                                                                                value={""}
-                                                                                onChange={(event) => {
-                                                                                    if (event.target.checked) {
-                                                                                        setSelectedObjectiveId(objective.id);
-                                                                                        setSelectedCombinedObjectiveId(objective.combinedObjectiveId);
-                                                                                    }
-                                                                                }}
-                                                                            />
-                                                                            <Typography>
-                                                                                {objective.objectiveName}
-                                                                            </Typography>
-                                                                        </Stack>
-                                                                    </div>
-                                                                ))}
-                                                                <Typography variant="h5" sx={{ my: 1 }}>
-                                                                    Enter Percentage of Success:
-                                                                </Typography>
-                                                                <TextField
-                                                                    multiline
-                                                                    fullWidth
-                                                                    label="Add a number from 1-100"
-                                                                    value={progress}
-                                                                    onChange={(event) => setProgress(event.target.value)}
-                                                                    variant="filled"
-                                                                    sx={{ my: 1 }}
-                                                                />
-                                                                <button className="add-button">
-                                                                    Add {student.name}'s Progess
-                                                                </button>
-                                                            </Stack>
-                                                        </form>
-                                                    </Card>
-                                                }
-                                            </Stack>
-                                        </Stack>
-                                    </Card>
-                                </div>
-                            ))}
-                        </Card>
+                        <ClassStudentsProgress id={id}/>
                     </Grid>
                 </Grid>
             </Card>
---
+++ src/client/Lessons/SingleLesson/components/AddProgress.jsx
@@ -0,0 +1,98 @@
+import Alert from "@mui/material/Alert"
+import Card from "@mui/material/Card"
+import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
+import TextField from "@mui/material/TextField"
+import { useState } from "react"
+import { usePostProgressMutation } from "../../../../redux/api"
+
+const AddProgress = ({ data, student }) => {
+    const [selectedStudentId, setSelectedStudentId] = useState("");
+    const [selectedObjectiveId, setSelectedObjectiveId] = useState("");
+    const [selectedCombinedObjectiveId, setSelectedCombinedObjectiveId] = useState("");
+    const [addError, setAddError] = useState(null);
+    const [progress, setProgress] = useState("");
+    const [addProgressMutation] = usePostProgressMutation();
+
+    const handleAddProgress = async (event) => {
+        try {
+            event.preventDefault();
+            const result = await addProgressMutation({
+                studentId: Number(selectedStudentId),
+                objectiveId: Number(selectedObjectiveId),
+                progressPercent: Number(progress),
+                combinedObjectiveId: Number(selectedCombinedObjectiveId)
+            });
+            if (result.data) {
+                setAddError(false);
+                setSelectedStudentId(null);
+                console.log("Success!");
+            } else {
+                setAddError(true);
+                console.log("Could not add progress");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    return (
+        <div>
+            <Stack direction="column">
+                <button
+                    style={{ width: 170 }}
+                    onClick={() => { setSelectedStudentId(student.id) }}
+                    className="details-button">
+                    Add New Progress
+                </button>
+                {selectedStudentId === student.id &&
+                    <Card
+                        sx={{ p: 1, m: 3 }}
+                        elevation={10}>
+                        <form onSubmit={handleAddProgress}>
+                            <Stack direction="column">
+                                <Typography variant="h5" sx={{ my: 1 }}>
+                                    Select Learning Objective:
+                                </Typography>
+                                {data.learningObjectives.map((objective) => (
+                                    <div key={objective.id}>
+                                        <Stack direction="row">
+                                            <input
+                                                type="checkbox"
+                                                value={""}
+                                                onChange={(event) => {
+                                                    if (event.target.checked) {
+                                                        setSelectedObjectiveId(objective.id);
+                                                        setSelectedCombinedObjectiveId(objective.combinedObjectiveId);
+                                                    }
+                                                }}
+                                            />
+                                            <Typography>
+                                                {objective.objectiveName}
+                                            </Typography>
+                                        </Stack>
+                                    </div>
+                                ))}
+                                <Typography variant="h5" sx={{ my: 1 }}>
+                                    Enter Percentage of Success:
+                                </Typography>
+                                <TextField
+                                    multiline
+                                    fullWidth
+                                    label="Add a number from 1-100"
+                                    value={progress}
+                                    onChange={(event) => setProgress(event.target.value)}
+                                    variant="filled"
+                                    sx={{ my: 1 }}
+                                />
+                                <button className="add-button">
+                                    Add {student.name}'s Progess
+                                </button>
+                            </Stack>
+                        </form>
+                    </Card>
+                }
+            </Stack>
+        </div>
+    )
+}
+export default AddProgress
