9f5f03ece5a2f1789f55885dc9e18234ca0e66b9
Kat Christensen <katchristensen336@gmail.com>
2024-03-14 17:56:52 -0400

    Remove stlyes to have smoother switch transition

--- src/client/Lessons/SingleLesson/components/AttendenceToggle.jsx
+++ src/client/Lessons/SingleLesson/components/AttendenceToggle.jsx
@@ -1,43 +1,13 @@
-import { styled } from '@mui/material/styles';
 import Switch from '@mui/material/Switch';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { useState } from 'react';
 import { useUpdateAttendenceMutation } from '../../../../redux/api';
+
 const AttendenceToggle = ({ studentData, studentId, lessonId }) => {
     const [attendance, setAttendence] = useState(studentData.attendances[0]?.present || false);
     const [updateAttendence] = useUpdateAttendenceMutation();
-    const Android12Switch = styled(Switch)(({ theme }) => ({
-        padding: 8,
-        '& .MuiSwitch-track': {
-            borderRadius: 22 / 2,
-            '&::before, &::after': {
-                content: '""',
-                position: 'absolute',
-                top: '50%',
-                transform: 'translateY(-50%)',
-                width: 16,
-                height: 16,
-            },
-            '&::before': {
-                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
-                    theme.palette.getContrastText(theme.palette.primary.main),
-                )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
-                left: 12,
-            },
-            '&::after': {
-                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
-                    theme.palette.getContrastText(theme.palette.primary.main),
-                )}" d="M19,13H5V11H19V13Z" /></svg>')`,
-                right: 12,
-            },
-        },
-        '& .MuiSwitch-thumb': {
-            boxShadow: 'none',
-            width: 20,
-            height: 20,
-        },
-    }));
+   
     return (
         <div>
             <Stack
@@ -46,7 +16,7 @@ const AttendenceToggle = ({ studentData, studentId, lessonId }) => {
                 spacing={1}
                 alignItems="center">
                 <Typography>absent</Typography>
-                <Android12Switch
+                <Switch
                     checked={attendance}
                     onChange={async () => {
                         const changeAttendance = !attendance;
@@ -58,7 +28,6 @@ const AttendenceToggle = ({ studentData, studentId, lessonId }) => {
                         setAttendence(changeAttendance);
                         console.log(updatedAttendance);
                     }}
-                    inputProps={{ 'aria-label': 'ant design' }}
                 />
                 <Typography>present</Typography>
             </Stack>
